186f056db97e8cc7dbff95dc66d0dc03e085805d
Hiwot Derese <Hiwotderese16@gmail.com>
2024-06-13 11:04:41 +0300

    change app launching icon

--- dozer_mobile/lib/main.dart
+++ dozer_mobile/lib/main.dart
@@ -1,20 +1,15 @@
 import 'package:dozer_mobile/core/routes/routes_name.dart';
 import 'package:dozer_mobile/core/routes/route.dart';
-import 'package:dozer_mobile/core/utils/get_storage_helper.dart';
 import 'package:dozer_mobile/dozer_exports.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:get_storage/get_storage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // await GetStorageHelper.addValue('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjNlZmQ1ZTIxLTk0MmUtNDFlOS1iMTMxLWEwYjAxYzgzOWVjNyIsImlhdCI6MTcxMzkzNTE5MCwiZXhwIjoxNzIxNzExMTkwfQ.dl5FC_V-bZhhukfuz-HW2-4qrgOzR4ccWmHH4H49Hs0');
   // await GetStorageHelper.clearAll();
-  await Firebase.initializeApp(
-  );
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
